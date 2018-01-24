a73536fd3aa10f63299ddeddf658503a7c83c7df
Timur Khazamov <t1mmaas@skbkontur.ru>
2018-01-25 01:20:04 +0500

    fixed FormControl width overflow issues

--- src/FormControl.js
+++ src/FormControl.js
@@ -5,8 +5,13 @@ type FormControlProps = {
   [key: string]: any
 };
 
-const FormControl = ({ style = {}, ...rest }: FormControlProps) => (
-  <span {...rest} style={{ ...style }} />
+const FormControl = ({ style, ...rest }: FormControlProps) => (
+  <span {...rest} style={getStyles(style)} />
 );
+
+const getStyles = (ownStyles = {}) => ({
+  flex: 1,
+  ...ownStyles
+});
 
 export default FormControl;

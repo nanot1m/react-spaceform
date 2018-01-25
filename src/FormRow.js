e830d4197cf864ff09caadaf01b39311348ce34e
Timur Khazamov <t1mmaas@skbkontur.ru>
2018-01-25 11:00:42 +0500

    Removed redunant margin from FormRow

--- src/FormRow.js
+++ src/FormRow.js
@@ -1,24 +1,32 @@
 // @flow
 import * as React from "react";
-
+import injectSheet from "react-jss";
 import { Consumer } from "./FormContext";
 
 type FormRowProps = {
   [key: string]: any
 };
 
-const FormRow = ({ style, ...rest }: FormRowProps) => (
+const styles = {
+  row: {
+    display: "flex",
+    flexWrap: "wrap",
+    alignItems: "baseline",
+    marginBottom: props => props.spacing * 2,
+    "&:last-child": {
+      marginBottom: 0
+    }
+  }
+};
+
+const StyledRow = injectSheet(styles)(({ classes, ...rest }) => (
+  <div className={classes.row} {...rest} />
+));
+
+const FormRow = (props: FormRowProps) => (
   <Consumer>
-    {({ spacing }) => <div {...rest} style={getStyles(spacing, style)} />}
+    {({ spacing }) => <StyledRow spacing={spacing} {...props} />}
   </Consumer>
 );
 
 export default FormRow;
-
-const getStyles = (spacing, ownStyles = {}) => ({
-  display: "flex",
-  flexWrap: "wrap",
-  alignItems: "baseline",
-  marginBottom: spacing * 2,
-  ...ownStyles
-});
